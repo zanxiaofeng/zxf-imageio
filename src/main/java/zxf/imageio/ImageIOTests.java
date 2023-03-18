02f5f9356598584ddcc2e902988fd6f99cb0ea75
davis <zanxiaofeng@163.com>
2023-03-18 18:09:20 +0800

    update

--- src/main/java/zxf/imageio/ImageIOTests.java
+++ src/main/java/zxf/imageio/ImageIOTests.java
@@ -7,21 +7,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class ImageIOTests {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args) {
         System.out.println("ReaderFormatNames:");
         Arrays.stream(ImageIO.getReaderFormatNames())
