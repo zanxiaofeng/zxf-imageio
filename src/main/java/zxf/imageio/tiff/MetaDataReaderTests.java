3f3e9629b25239cb5ba38256376ba284446748c8
davis <zanxiaofeng@163.com>
2023-08-10 09:17:31 +0800

    update

--- src/main/java/zxf/imageio/tiff/MetaDataReaderTests.java
+++ src/main/java/zxf/imageio/tiff/MetaDataReaderTests.java
@@ -8,15 +8,14 @@ import com.drew.metadata.Tag;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Iterator;
 
 public class MetaDataReaderTests {
     public static void main(String[] args) throws TiffProcessingException, IOException {
         Metadata metadata = TiffMetadataReader.readMetadata(Paths.get("./output/developer-mozilla-org-CORS-zh.tiff").toFile());
-        System.out.println(metadata);
 
         Iterable<Directory> directories = metadata.getDirectories();
         for (Directory directory : directories) {
+            System.out.println(directory);
             for (Tag tag : directory.getTags()) {
                 System.out.println(tag);
             }
